6ecc45e689cf413aabfc65fe5de3bbd64f42769c
LucianoBaldoFagioli <lbfagioli@miuandes.cl>
2024-03-11 15:40:10 -0300

    third commit

---
+++ jvs.js
@@ -0,0 +1,8 @@
+const buttonHandler = event => {
+    div.style.backgroundColor = text.value;
+}
+
+const button = document.getElementById("submit-button");
+const text = document.getElementById("mytext");
+const div = document.getElementById("change");
+button.addEventListener("click", buttonHandler);
