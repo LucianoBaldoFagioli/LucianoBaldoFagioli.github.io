f2b0fd74d7ee31edc62f1ed03fabab926facee41
LucianoBaldoFagioli <lbfagioli@miuandes.cl>
2024-03-11 16:09:58 -0300

    fifth commit

--- jvs.js
+++ jvs.js
@@ -6,3 +6,5 @@ const button = document.getElementById("submit-button");
 const text = document.getElementById("mytext");
 const div = document.getElementById("change");
 button.addEventListener("click", buttonHandler);
+
+// final commit
